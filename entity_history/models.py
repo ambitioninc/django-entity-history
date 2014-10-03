23bbaa6a187cb9bf1ae3679c4457533c48844d06
Wes Kendall <wes.kendall@tryambition.com>
2014-10-03 13:42:14 -0400

    added models for entity events

---
+++ entity_history/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from entity.models import Entity
+
+
+class EntityActivationEvent(models.Model):
+    """
+    Models an event of an entity being activated or deactivated.
+    """
+    entity = models.ForeignKey(Entity, help_text='The entity that was activated / deactivated')
+    time = models.DateTimeField(help_text='The time of the activation / deactivation')
+    was_activated = models.BooleanField(help_text='True if the entity was activated, false otherwise')
