ecc280df3cfc5972068b64b0ca926d0a64e337a8
Wes Okes <wes.okes@gmail.com>
2019-07-11 16:36:31 -0400

    env

--- setup.py
+++ setup.py
@@ -36,15 +36,14 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Framework :: Django',
-        'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
     ],
     license='MIT',
     install_requires=[
-        'Django>=1.11',
-        'django-entity>=2.0.0',
+        'Django>=2.0',
+        'django-entity>=4.2.0',
         'psycopg2'
     ],
     tests_require=[
